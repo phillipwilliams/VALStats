e6b847fc7765dd311d4f8769075622b02f4a952c
phillip.williams <phillip.williams@wabtec.com>
2022-06-09 21:28:32 -0400

    Register & Sign in functionality
    Added functionality for the user to create an account or sign in to their account. This links with the firebase authentication DB. Also includes error checking for incorrect user input to the boxes.

--- vue-app/valfront/src/components/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="home">
-    <h3> hello</h3>
-    </div>
-</template>
-
-<script>
-
-</script>
---
+++ vue-app/valfront/src/views/SignIn.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Sign In to your Account</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Password" v-model="password" /></p>
+  <p v-if="errMsg">{{ errMsg}}</p>
+  <p><button @click="register">Submit</button></p>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+  import { useRouter } from 'vue-router';
+import router from '../router';
+  const email = ref('');
+  const password = ref('');
+  const errMsg = ref();
+  const register = () => {
+      signInWithEmailAndPassword(getAuth(), email.value, password.value)
+        .then((data) => {
+            console.log("Successfully registered!");
+            router.push('/feed')
+        })
+        .catch((error) =>{
+            console.log(error.code);
+            switch (error.code){
+                case "auth/invalid-email":
+                    errMsg.value = "Invalid email";
+                    break;
+                case "auth/user-not-found":
+                    errMsg.value = "No account with that email was found";
+                    break;
+                case "auth/wrong-password":
+                    errMsg.value = "Incorrect Password";
+                    break;
+                default:
+                    errMsg.value = "Email or password incorrect";
+                    break;
+               
+            }
+            
+        });
+  };
+</script>
