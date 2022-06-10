5dfee74fbc535eca14b00cfb52b6fb6c5b3de024
phillip.williams <phillip.williams@wabtec.com>
2022-06-09 21:26:44 -0400

    Change to the main page styling + user authentication
    Changes to the layout of the App.vue main single page. Also includes functionality for knowing if the user is signed in or not.

--- vue-app/valfront/src/App.vue
+++ vue-app/valfront/src/App.vue
@@ -1,103 +1,57 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Navigation from './components/Nav.vue'
-import Login from './components/Login.vue'
+import { onMounted, ref } from 'vue';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { routeLocationKey } from 'vue-router';
+import router from './router';
 
+const isLoggedIn = ref(false);
+let auth;
+onMounted(() =>{
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) =>{
+    if (user){
+      isLoggedIn.value = true;
+    }
+    else{
+      isLoggedIn.value = false;
+    }
+  });
+});
 
-
+const handleSignOut = () => {
+  signOut(auth).then(() =>{
+    router.push("/");
+  })
+}
 
 </script>
 
 <template>
-  <div id="app">
-  
-  <Navigation />
-  <div class="auth-wrapper">
-    <div class="auth-inner">
-      <Login/>
-    </div>
-  </div>
+  <div>
+    <nav>
+      <router-link to="/"> Home </router-link> |
+<<<<<<< Updated upstream
+      <router-link to="/feed"> Feed </router-link> |
+=======
+      <router-link to="/feed"> Profile </router-link> |
+>>>>>>> Stashed changes
+      <router-link to="/register"> Register </router-link> |
+      <router-link to="/sign-in"> Login </router-link> |
+      <button @click="handleSignOut" v-if="isLoggedIn">Sign out</button>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <style>
-    @import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800');
-
-    * {
-      box-sizing: border-box;
-    }
-
-    body{
-      background: #1C8EF9 !important;
-      min-height: 100vh;
-      display: flex;
-      font-weight: 400;
-      font-family: 'Fira Sans', sans-serif;
-    }
-
-    h1, h2, h3, h4, h5, h6, label, span {
-      font-weight: 500;
-      font-family: 'Fira Sans', sans-serif;
-    }
-
-    body, html, #app, #root, .auth-wrapper {
-      width: 100%;
-      height: 100%;
-    }
-
-    #app{
-      text-align: center;
-    }
-
-    .navbar-light {
-      background-color: #ffffffff;
-      box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
-    }
-
-    .auth-wrapper {
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      text-align: left;
-    }
-
-    .auth-inner {
-      width: 450px;
-      margin: auto;
-      background: #ffffffff;
-      box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
-      padding: 30px 55px 45px 55px;
-      border-radius: 15px;
-      transition: all .3s;
-    }
-
-    .auth-wrapper .form-control:focus {
-      border-color: #167bff;
-      box-shadow: none;
-    }
-
-    .auth-wrapper h3 {
-      text-align: center;
-      margin: 0;
-      line-height: 1;
-      padding-bottom: 20px;
-    }
-
-    .custom-control-label {
-      font-weight: 400;
-    }
-
-    .forgot-password,
-    .forgot-password a {
-      text-align: right;
-      font-size: 13px;
-      padding-top: 10px;
-      color: #7f7d7d;
-      margin: 0;
-    }
-
-    .forgot-password a {
-      color: #167bff;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
