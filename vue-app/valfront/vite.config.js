cb0d88aac198830cdf6b30ee5a8307a786206838
phillipwilliams <45475801+phillipwilliams@users.noreply.github.com>
2022-06-19 01:01:11 -0400

    Updates to vite.config
    Testing fails if base: '/VALStats/' is present in vite.config.js

--- vue-app/valfront/vite.config.js
+++ vue-app/valfront/vite.config.js
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VALStats/',
+  
   test: {
     globals: true,
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  
 })
